b74ce603156eca064652ae6f94c402e4a5ff3992
sneha039 <123440006+sneha039@users.noreply.github.com>
2023-02-25 20:55:01 +0530

    Add files via upload

---
+++ Practice_SET_01.py
@@ -0,0 +1,21 @@
+# print("Hello",end=' '"W")
+# print("Hello",end='-'"W")
+# print("Hello",end=','"W")
+
+# print("Hello",end=' '"W")
+# print("\nHello",end='-'"W")
+# print("\nHello",end=','"W")
+
+# print("Hello",end=''"Welcome to Josh innovations\n")
+
+# print("Python Programming",end=",")
+# print("From Josh Innovations",end=".")
+# print("Course: Core Python Programming")
+
+# print("Hello IPL on 26th MARCH","2 Days to go",sep='-')
+# print("Hello",sep=".""Josh Innovations")
+# print("Core","Python","Programming",sep="*")
+
+# print("Ameerpet Metro Station",end=".")
+# print("Near SAP Street Lane",end="\n")
+# print("Josh","Innovations",end='.',sep="-")
